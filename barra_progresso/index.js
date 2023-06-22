722fae4e15deadc38089219a9365d442c6e87957
Lila <dalila.analistadesistema@gmail.com>
2023-06-22 20:35:23 -0300

    load

---
+++ barra_progresso/index.js
@@ -0,0 +1,20 @@
+var counter = 0;
+
+window.addEventListener("DOMContentLoaded",move=() => {
+  if (counter == 0) {
+    j = 1;
+    var elem = document.querySelector(".progresso-done");
+
+    var width = 50;
+    var main = setInterval(frame,50);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(main);
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+});
